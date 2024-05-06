993685f3773b4edfe10fa440fa307a891a91d6a1
Kerzhakov Nikita Alexeevich <nikitakerzhik@gmail.com>
2024-05-06 13:56:17 +1000

    Create model and added migrations for app notification

--- ural/notification/models.py
+++ ural/notification/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from user_app.models import User
 
-# Create your models here.
+MAYBECHOICE = (
+    ('y', 'Yes'),
+    ('n', 'No'),
+    ('u', 'Unknown'),
+)
+class Notify(models.Model):
+    name = models.TextField()
+    first_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='first_user_notifications')
+    second_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='second_user_notifications')
+    status = models.CharField(max_length=1, choices=MAYBECHOICE)
+
+    class Meta:
+        db_table='notify'
