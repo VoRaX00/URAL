eaf10068eb8b65e4690611b0823950c55839d6c5
Kerzhakov Nikita Alexeevich <nikitakerzhik@gmail.com>
2024-05-12 15:14:19 +1000

    Updated sending data to the notification and display page

--- ural/car_cargo/views.py
+++ ural/car_cargo/views.py
@@ -35,8 +35,7 @@ def addCargo(request):
             bcashless=cashless, bcashless_nds=nds, bcashless_without_nds=without_nds, price_cash=request.POST['deliveryCostCash'],
             price_cash_nds = request.POST['deliveryCostNDS'], price_cash_without_nds = request.POST['deliveryCostWithoutNDS'], 
             request_price=request_price, comment = request.POST['comment'], user_id = request.user)
-        
-        
+         
         cargo.save()
     return render(request, 'addCargo.html')
 
@@ -49,7 +48,6 @@ def addCar(request):
                 ready_from = request.POST['readyFrom'], phone = request.POST['phone'], comment = request.POST['comment'], user_id=id)
         car.save()
 
-        
         typesBody = request.POST.getlist('bodyType')
         for i in typesBody:
             type_body_instance = typeBody.objects.get(name=i)
@@ -93,27 +91,20 @@ class VCar:
         self.type_body = []
         self.type_loading = []
 
-
-def viewCar(request):
-    #cars = carTypeBody.objects.all().select_related('car__user', 'type_body').order_by('-id')
-    all_cars = Car.objects.all().select_related('user').order_by('-id')
-    cars = []
-    
+def arrayCars(all_cars):
+    cars=[]
     for i in all_cars:
         car = VCar(i.id, i.car, i.capacity, i.volume, i.length, i.width, i.height, i.where_from, i.where, i.ready_from, i.ready_to, i.phone, i.comment,)
-        
         typesBody = carTypeBody.objects.all().select_related('car', 'type_body')
         check = False
         j=0
         k=0
         while j < len(typesBody):
             if typesBody[j].car.id == i.id:
-                #print(typesBody[j].type_body.name)
                 car.type_body.append(typesBody[j].type_body.name)
                 check = True
             elif check:
                 break
-                
             j+=1
 
         check = False
@@ -126,8 +117,12 @@ def viewCar(request):
                 break
                 
             k+=1
-
         cars.append(car)
+    return cars
+
+def viewCar(request):
+    all_cars = Car.objects.all().select_related('user').order_by('-id')
+    cars = arrayCars(all_cars)
 
     paginator = Paginator(cars, per_page=4)
     page_number = request.GET.get('page')
@@ -139,77 +134,91 @@ def viewCar(request):
     
     return render(request, 'viewCar.html', context=context)
 
+#не введено пока что в основной проект
+def editCar(post): 
+    car_id = post['car_id']
+    car = Car.objects.get(id=car_id)
+    car.car = post['car']
+    car.capacity = post['capacity']
+    car.volume = post['volume']
+    car.length = post['length']
+    car.width = post['width']
+    car.height = post['height']
+    car.where_from = post['place_from']
+    car.where = post['place_to']
+    car.ready_to = post['readyTo']
+    car.ready_from = post['readyFrom']
+    car.phone = post['phone']
+    car.comment = post['comment']
+    #car.save()
 
+    #дописать удаление всех типов кузова и потом добавление новых типов кузова
+    typesBody = post.getlist('bodyType')
+    for i in typesBody:
+        type_body_instance = typeBody.objects.get(name=i)
+        car_type_body = carTypeBody(car=car, type_body=type_body_instance)
+        #car_type_body.save()
 
+    typesLoading = post.getlist('loadingType')
+    for i in typesLoading:
+        type_loading_instance = typeLoading.objects.get(name=i)
+        car_type_loading = carTypeLoading(car=car, type_loading=type_loading_instance)
+        #car_type_loading.save()
+
+def editCargo(post):
+    cargo_id = post['cargo_id']
+        
+    try:
+        cargo = Cargo.objects.get(id=cargo_id)
+
+        arrCash = post.getlist('cash')
+        if 'cash' in arrCash:
+            cargo.bcash = True
+
+        if 'cashless' in arrCash:
+            cargo.bcashless = True
+            arrCash = post.getlist('cashless')
+            if 'nds' in arrCash:
+                cargo.bcashless_nds=True
+            if 'without_nds' in arrCash:
+                cargo.bcashless_without_nds=True
+        
+        arrCash = post.getlist('request_price')
+
+        if len(arrCash) != 0:
+            cargo.request_price = True
+
+        cargo.name = post['cargoName']
+        cargo.length = post['length']
+        cargo.width = post['width']
+        cargo.height = post['height']
+        cargo.weight = post['cargoWeight']
+        cargo.volume = post['volume']
+        cargo.count_place = post['countPlace']
+        cargo.loading_data = post['loadingDate']
+        cargo.unloading_data = post['unloadingDate']
+        cargo.phone = post['phone']
+        cargo.loading_place = post['loading_address']
+        cargo.unloading_place = post['unloading_address']
+        cargo.price_cash = post['deliveryCostCash']
+        cargo.price_cash_nds = post['deliveryCostNDS']
+        cargo.price_cash_without_nds = post['deliveryCostWithoutNDS']
+        cargo.comment = post['comment']
+    
+        cargo.save()
+
+    except ObjectDoesNotExist:
+        return HttpResponseForbidden("груза с таким id не существует")
 
 def myCar(request):
-    #cars = carTypeBody.objects.all().select_related('car__user', 'type_body').order_by('-id')
     if request.POST:
         csrf_token = request.POST.get('csrfmiddlewaretoken')
         if not csrf_token == request.COOKIES.get('csrfmiddlewaretoken'):
             return HttpResponseForbidden("CSRF Token не действителен.")
+        editCar(request.POST)
         
-        car_id = request.POST['car_id']
-        car = Car.objects.get(id=car_id)
-        car.car = request.POST['car']
-        car.capacity = request.POST['capacity']
-        car.volume = request.POST['volume']
-        car.length = request.POST['length']
-        car.width = request.POST['width']
-        car.height = request.POST['height']
-        car.where_from=request.POST['place_from']
-        car.where=request.POST['place_to']
-        car.ready_to = request.POST['readyTo']
-        car.ready_from = request.POST['readyFrom']
-        car.phone = request.POST['phone']
-        car.comment = request.POST['comment']
-        #car.save()
-
-        #дописать удаление всех типов кузова и потом добавление новых типов кузова
-        typesBody = request.POST.getlist('bodyType')
-        for i in typesBody:
-            type_body_instance = typeBody.objects.get(name=i)
-            car_type_body = carTypeBody(car=car, type_body=type_body_instance)
-            #car_type_body.save()
-
-        typesLoading = request.POST.getlist('loadingType')
-        for i in typesLoading:
-            type_loading_instance = typeLoading.objects.get(name=i)
-            car_type_loading = carTypeLoading(car=car, type_loading=type_loading_instance)
-            #car_type_loading.save()
-
     all_cars = Car.objects.all().select_related('user').filter(user_id=request.user).order_by('-id')
-    cars = []
-    
-    for i in all_cars:
-        car = VCar(i.id, i.car, i.capacity, i.volume, i.length, i.width, i.height, i.where_from, i.where, i.ready_from, i.ready_to, i.phone, i.comment,)
-        
-        typesBody = carTypeBody.objects.all().select_related('car', 'type_body')
-        check = False
-        j=0
-        k=0
-        while j < len(typesBody):
-            if typesBody[j].car.id == i.id:
-                #print(typesBody[j].type_body.name)
-                car.type_body.append(typesBody[j].type_body.name)
-                check = True
-            elif check:
-                break
-                
-            j+=1
-
-        check = False
-        typesLoading = carTypeLoading.objects.all().select_related('car', 'type_loading')
-        while k < len(typesLoading):
-            if typesLoading[k].car.id == i.id:
-                car.type_loading.append(typesLoading[k].type_loading.name)
-                check=True
-            elif check:
-                break
-                
-            k+=1
-
-        cars.append(car)
+    cars = arrayCars(all_cars)
 
     paginator = Paginator(cars, per_page=4)
     page_number = request.GET.get('page')
@@ -223,49 +232,7 @@ def myCar(request):
 
 def myCargo(request):
     if request.POST:
-
-        cargo_id = request.POST['cargo_id']
-        
-        try:
-            cargo = Cargo.objects.get(id=cargo_id)
-            if cargo.user_id != request.user:
-                return HttpResponseForbidden("у вас нет прав")
-
-            arrCash = request.POST.getlist('cash')
-            if 'cash' in arrCash:
-                cargo.bcash = True
-            if 'cashless' in arrCash:
-                cargo.bcashless = True
-                arrCash = request.POST.getlist('cashless')
-                if 'nds' in arrCash:
-                    cargo.bcashless_nds=True
-                if 'without_nds' in arrCash:
-                    cargo.bcashless_without_nds=True
-            
-            arrCash = request.POST.getlist('request_price')
-            if len(arrCash) != 0:
-                cargo.request_price = True
-
-            cargo.name = request.POST['cargoName']
-            cargo.length = request.POST['length']
-            cargo.width = request.POST['width']
-            cargo.height = request.POST['height']
-            cargo.weight = request.POST['cargoWeight']
-            cargo.volume = request.POST['volume']
-            cargo.count_place = request.POST['countPlace']
-            cargo.loading_data = request.POST['loadingDate']
-            cargo.unloading_data = request.POST['unloadingDate']
-            cargo.phone = request.POST['phone']
-            cargo.loading_place = request.POST['loading_address']
-            cargo.unloading_place = request.POST['unloading_address']
-            cargo.price_cash=request.POST['deliveryCostCash']
-            cargo.price_cash_nds = request.POST['deliveryCostNDS']
-            cargo.price_cash_without_nds = request.POST['deliveryCostWithoutNDS']
-            cargo.comment = request.POST['comment']
-        
-            cargo.save()
-        except ObjectDoesNotExist:
-            return HttpResponseForbidden("груза с таким id не существует")
+        editCargo(request.POST) 
 
     cargs = Cargo.objects.all().select_related('user_id').filter(user_id=request.user).order_by('-id')
     paginator = Paginator(cargs, per_page=4)
